a0389c4f623a4f85c7e3f17a56a18253c0cc992d
Eric Kersten <ekersten@gmail.com>
2013-11-26 23:13:56 -0300

    Partial thumbnail implementation

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
+
+from moar import Thumbnailer
 
 from .models import Painting
 
 def home(request):
-	return HttpResponse('Holis')
+	paintings = Painting.objects.all()
+	thumbnail = Thumbnailer()
+	return render_to_response('website/index.html', {'paintings':paintings, 'thumbnail':thumbnail}, context_instance=RequestContext(request))
