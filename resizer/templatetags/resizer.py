b004d2340fe846c0d6d13de3f0f4b4d7605409b0
vagrant <vagrant@vagrant-debiani386>
2014-02-05 16:28:51 -0300

    finished resizer code

--- resizer/templatetags/resizer.py
+++ resizer/templatetags/resizer.py
@@ -18,6 +18,9 @@ def do_resizer(parser, token):
 
 
 class ResizerNode(template.Node):
+	WIDTH = 'width'
+	HEIGHT = 'height'
+
 	def __init__(self, image, format, mode):
 		try:
 			self.image = template.Variable(image)
@@ -42,15 +45,43 @@ class ResizerNode(template.Node):
 		resized_name = self.get_name(basename, self.format, self.mode)
 		resized_path = os.path.join(self.path, resized_name)
 
+		final_width = self.dimensions['width']
+		final_height = self.dimensions['height']
+
 		if (os.path.exists(resized_path) == False):
 			# check if file exists
 			if (os.path.exists(img_obj.path)):
-				img = Image.open(img_obj.path)
+				newimg = Image.open(img_obj.path)
+
 				if (self.mode == 'crop'):
-					pass
+					if (img_obj.width/self.dimensions['width']>img_obj.height/self.dimensions['height']):
+						master =  ResizerNode.HEIGHT
+					else:
+						master = ResizerNode.WIDTH
 				else:
-					nueva = img.resize((self.dimensions['width'], self.dimensions['height']), Image.ANTIALIAS)
-					nueva.save(resized_path)
+					if (img_obj.width/self.dimensions['width']>img_obj.height/self.dimensions['height']):
+						master = ResizerNode.WIDTH
+					else:
+						master =  ResizerNode.HEIGHT
+
+				if (master == ResizerNode.WIDTH):
+					final_height = img_obj.height * final_width / img_obj.width
+				elif (master == ResizerNode.HEIGHT):
+					final_width = img_obj.width * final_height / img_obj.height
+
+				final_width = int(round(final_width))
+				final_height = int(round(final_height))
+
+				newimg = newimg.resize((final_width, final_height), Image.ANTIALIAS)
+
+				if (self.mode == 'crop'):
+					left = int(round((final_width - self.dimensions['width']) / 2))
+					top = int(round((final_height - self.dimensions['height']) / 2))
+					right = left + self.dimensions['width']
+					bottom = top + self.dimensions['height']
+					newimg = newimg.crop((left, top, right, bottom))
+
+				newimg.save(resized_path)
 
 			else:
 				raise template.TemplateSyntaxError("%r path does not exist" % img_obj.path)
